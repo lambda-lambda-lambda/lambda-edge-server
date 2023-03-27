74d6917d85717d599f92402dda7f13e87181541f
Marc S. Brooks <marc@mbrooks.info>
2023-03-27 09:26:05 -0700

    Fixed response code alignment

--- test/handler.js
+++ test/handler.js
@@ -1,19 +1,19 @@
 /**
  * @see AWS::Serverless::Function
  */
-exports.handler = function (event, context, callback) {
+exports.handler = function(event, context, callback) {
   const response = {
     status: '200',
     statusDescription: 'OK',
     headers: {
-        'cache-control': [{
-            key: 'Cache-Control',
-            value: 'max-age=0'
-        }],
-        'content-type': [{
-            key: 'Content-Type',
-            value: 'text/html'
-        }]
+      'cache-control': [{
+          key: 'Cache-Control',
+          value: 'max-age=0'
+      }],
+      'content-type': [{
+          key: 'Content-Type',
+          value: 'text/html'
+      }]
     },
     body: 'Success',
   };
